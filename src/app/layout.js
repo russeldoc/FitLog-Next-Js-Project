5e7dbfde0ff8ba986e72ee1297eac737d76d8d3a
Noor Ahmed <russeldoc@gmail.com>
2026-09-25 11:45:16 +0600

    Created the FitLog Project

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,11 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <h2 className="font-bold text-5xl text-center">Navbar</h2>
+        {children}
+        <h2 className="font-bold text-5xl text-center">Footer</h2>
+      </body>
     </html>
   );
 }
